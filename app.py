8132e4dff82bc3177ed24af4af57f307cc298112
befan3110 <stefje@outlook.dk>
2025-09-12 11:40:28 +0200

    personalized the work

--- app.py
+++ app.py
@@ -5,41 +5,45 @@ import sqlite3
 app = Flask(__name__)
 DB_DND = "DND.db"
 DB_DND2 = "DND2.db"
-def get_db_albums():
-    if "db" not in g:
-        g.db = sqlite3.connect(DB_DND2)
-        g.db.row_factory = sqlite3.Row  # optional, for dict-like rows
-    return g.db
 
-def get_db_users():
-    if "db" not in g:
-        g.db = sqlite3.connect(DB_DND)
-        g.db.row_factory = sqlite3.Row  # optional, for dict-like rows
-    return g.db
+def get_db_classes():
+    if "db_classes" not in g:
+        g.db_classes = sqlite3.connect(DB_DND2)
+        g.db_classes.row_factory = sqlite3.Row  # optional, for dict-like rows
+    return g.db_classes
 
+def get_db_spells():
+    if "db_spells" not in g:
+        g.db_spells = sqlite3.connect(DB_DND)
+        g.db_spells.row_factory = sqlite3.Row  # optional, for dict-like rows
+    return g.db_spells
 
 @app.teardown_appcontext
 def close_db(exception):
-    db = g.pop("db", None)
-    if db is not None:
-        db.close()
+    db_classes = g.pop("db_classes", None)
+    if db_classes is not None:
+        db_classes.close()
+
+    db_spells = g.pop("db_spells", None)
+    if db_spells is not None:
+        db_spells.close()
 
 # Define the main route for the application
-@app.route("/beatles")
-def beatles_page():
-    db = get_db_albums()
-    cur = db.execute("SELECT title, yyear FROM album")
+@app.route("/DND_classes")
+def classes_page():
+    classes_db = get_db_classes()
+    cur = classes_db.execute("SELECT class, descriptor FROM classes")
     data = cur.fetchall()
     members = {"members": [dict(u) for u in data]}
-    return render_template("albums.html", title="Welcome", members=members)
+    return render_template("classes.html", title="Welcome", members=members)
 
-@app.route("/")
-def front_page():
-    user_db = get_db_users()
-    cur = user_db.execute("SELECT uid, uname, password FROM user")
+@app.route("/DND_spells")
+def spells_page():
+    spells_db = get_db_spells()
+    cur = spells_db.execute("SELECT name, level, school FROM spells")
     data = cur.fetchall()
     users = {"users": [dict(u) for u in data]}
-    return render_template("index.html", title="Welcome", users=users)
+    return render_template("spells.html", title="Welcome", users=users)
     
 # Start Flask server
 if __name__ == "__main__":
